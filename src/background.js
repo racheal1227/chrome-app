6575d765d6acde2de6ad2493a58e9d33c08c44a1
백지현 <racheal1227@gmail.com>
2022-10-20 10:30:35 +0900

    경로 변경

--- src/background.js
+++ src/background.js
@@ -1,4 +1,4 @@
 const images = ['0.jpg', '1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg'];
 const image = images[Math.floor(Math.random() * images.length)];
 
-document.body.style.backgroundImage = `url('../image/${image}')`;
+document.body.style.backgroundImage = `url('image/${image}')`;
